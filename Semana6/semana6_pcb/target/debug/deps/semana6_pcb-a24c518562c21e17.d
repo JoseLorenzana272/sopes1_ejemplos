1b827e7152bac309b97d361a5fae7a221312211f
JoseLorenzana272 <jodanielorenzana@gmail.com>
2026-01-29 22:29:00 -0600

    feat: :art: fixes

--- Semana6/semana6_pcb/target/debug/deps/semana6_pcb-a24c518562c21e17.d
+++
@@ -1,5 +0,0 @@
-/home/jose/Desktop/Coding-3.0/sopes1_ejemplos/Semana6/semana6_pcb/target/debug/deps/semana6_pcb-a24c518562c21e17: src/main.rs
-
-/home/jose/Desktop/Coding-3.0/sopes1_ejemplos/Semana6/semana6_pcb/target/debug/deps/semana6_pcb-a24c518562c21e17.d: src/main.rs
-
-src/main.rs:
--- Semana6/semana6_pcb/target/debug/semana6_pcb.d
+++
@@ -1,1 +0,0 @@
-/home/jose/Desktop/Coding-3.0/sopes1_ejemplos/Semana6/semana6_pcb/target/debug/semana6_pcb: /home/jose/Desktop/Coding-3.0/sopes1_ejemplos/Semana6/semana6_pcb/src/main.rs
